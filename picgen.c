0ff3c82db1013957ecd5f87944881ea25bb27977
GarveyZhou <gzhou2@stuy.edu>
2019-01-31 21:40:47 -0500

    fix picgen

--- picgen.c
+++ picgen.c
@@ -2,18 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(){
 	int file =  open("pic.ppm",O_WRONLY | O_CREAT | O_TRUNC,0644);
 	char * line = "P3 500 500 255\n";
 	write(file,line,strlen(line));
-	char color[3];
+	int r,g,b;
+	char * buffer = malloc(20*sizeof(char));
 	for(int i = 0; i < 500; i++){
 		for(int j = 0;j<500;j++){
-			color[0] = i % 256;
-			color[1] = j % 256;
-			color[2] = (i + j) % 256;
-			write(file,color,strlen(color));
+			r = i % 256;
+			g = j % 256;
+			b = (i + j) % 256;
+			sprintf(buffer,"%d %d %d ",r,g,b);
+			write(file,buffer,strlen(buffer));
 		}
 	}
 	close(file);
